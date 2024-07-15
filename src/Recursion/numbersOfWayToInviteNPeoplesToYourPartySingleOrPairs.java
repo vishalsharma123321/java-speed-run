b7fcaf24b7e83edca1f50f8cf54e2ac11d555028
vishalsharma123321 <vishalsharma2212003@gmail.com>
2024-07-16 03:15:21 +0530

    change fixed

---
+++ src/Recursion/numbersOfWayToInviteNPeoplesToYourPartySingleOrPairs.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class numbersOfWayToInviteNPeoplesToYourPartySingleOrPairs {
+
+    static int recursion(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int way1 = recursion(n - 1);
+        int way2 = (n - 1) * recursion(n - 2);
+
+        return way1 + way2;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(recursion(n));
+    }
+}
